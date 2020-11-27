5c3942b15ae69666252dae23ee6ac7af3e8ddeea
BIN <fantasy_ice@163.com>
2020-11-27 08:49:53 +0800

    Create prod.js

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hostname: 'h5.zuche.com/static', // 将要替换的域名
+    domain: '//h5.zuche.com/static/', // 替换后域名
+    apiDomain: '//account.zuche.com/mapi',
+    apiVersion: {
+      zuche: 551,
+    },
+    needTrack: true
+  };
+  
