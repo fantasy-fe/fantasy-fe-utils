4924c0a0c2aa3f5502279e412bf784cfc070aa7a
BIN <fantasy_ice@163.com>
2020-11-27 08:49:06 +0800

    Update default.js

--- config/default.js
+++ config/default.js
@@ -1,3 +1,35 @@
-export default {
-  domain: 'http://xxx'
-}
+const ip = require('ip').address();
+const PORT = 9001;
+let cookie;
+const API_DOMAIN = 'http://localhost:8080';
+// const API_DOMAIN = 'http://h5test.zuche.com';
+module.exports = {
+  port: PORT,
+  hostname: `${ip}:${PORT}/`, // combo 将要替换的域名
+  domain: `//${ip}:${PORT}/`, // 替换后域名
+  apiDomain: `//${ip}:${PORT}/mapi`,
+  debugUID: '', // 本地调试uid
+  proxy: {
+    '/mapi/*': {
+      target: API_DOMAIN,
+      changeOrigin: true,
+      onProxyRes(proxyRes, req, res) {
+        const cookies = proxyRes.headers['set-cookie'];
+        if (!cookie) {
+          cookie = cookies;
+          // TODO 如有其他cookie需求再做filter
+        }
+      },
+      onProxyReq(proxyReq) {
+        if (cookie) {
+          proxyReq.setHeader('Cookie', cookie);
+        }
+      }
+    }
+  },
+  isOpenBrowser: true,
+  apiVersion: {
+    zuche: 551,
+  },
+  needTrack: false
+};
