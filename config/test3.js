32add62bddd7d105aa8441d1bb29d85e4f5ff09d
BIN <fantasy_ice@163.com>
2020-11-27 08:50:23 +0800

    Create test3.js

---
+++ config/test3.js
@@ -0,0 +1,10 @@
+module.exports = {
+    hostname: 'h5test03.zuche.com/static', // 将要替换的域名
+    domain: '//h5test03.zuche.com/static/', // 替换后域名
+    apiDomain: '//h5test03.zuche.com/mapi',
+    apiVersion: {
+      zuche: 551,
+    },
+    needTrack: false
+  };
+  
